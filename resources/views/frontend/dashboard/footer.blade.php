effd8aa7a4134d829aa84dd3775a92bf2a4ba360
Manzur Ahammed <manzur.ahammed236@gmail.com>
2020-02-18 23:57:20 +0600

    add dashboard

---
+++ resources/views/frontend/dashboard/footer.blade.php
@@ -0,0 +1,108 @@
+<!-- Footer -->
+<footer style="background: url('{{ asset("images/frontend/footer-bg.jpg") }}')" class="padding-top-100 footer-bg">
+    <div class="footer-widget-wrap padding-bottom-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget footer-about">
+                        <div class="logo">
+                            <img src="{{ asset('images/frontend/footer-logo.png') }}" class="img-fluid" alt="">
+                        </div>
+                        <p class="copyright-text">© 2019 <a href="#">Listereon</a>. By <a href="#">CodePassergers</a> All Rights Reserved</p>
+                        <div class="social-links">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget footer-widget-links">
+                        <h5 class="footer-widget-title">Quick Links</h5>
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Pricing Table</a></li>
+                            <li><a href="#">Recent Events</a></li>
+                            <li><a href="#">Latest News & Tops</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget footer-widget-links">
+                        <h5 class="footer-widget-title">My Account</h5>
+                        <ul>
+                            <li><a href="#">Dashboad</a></li>
+                            <li><a href="#">Listings</a></li>
+                            <li><a href="#">Favorites Listings</a></li>
+                            <li><a href="#">My Cart</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget footer-widget-contact">
+                        <h5 class="footer-widget-title">Contact Us</h5>
+                        <p class="office-address">9000 Regency Parkway, Suite 400 Cary, NC 27518</p>
+                        <p><span>E:</span> supportsaasapp@gmail.com <br> <span>P:</span> +000 1456 6978 111</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-2 order-lg-2">
+                    <div class="apps-download">
+                        <a href="#" class="app-download apple">
+                            <img src="{{ asset('images/frontend/apple.png') }}" class="img-fluid" alt="">
+                        </a>
+                        <a href="#" class="app-download android">
+                            <img src="{{ asset('images/frontend/android.png') }}" class="img-fluid" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-5 order-lg-1">
+                    <ul class="footer-nav">
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-5 order-lg-3">
+                    <div class="backtotop">
+                        <a href="#">Back to Top<i class="fas fa-angle-up"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
+
+<script src="{{ asset('css/frontend/js/jquery.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/popper.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/bootstrap.min.js') }}"></script>
+
+
+<script src="{{ asset('css/frontend/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/slick.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/plyr.js') }}"></script>
+<script src="{{ asset('css/frontend/js/jquery.timepicker.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/jquery.countTo.js') }}"></script>
+<script src="{{ asset('css/frontend/js/visible.js') }}"></script>
+<script src="{{ asset('css/frontend/js/datepicker.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/select2.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/datepicker.en.js') }}"></script>
+<script src="{{ asset('css/frontend/js/tinymce.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/jquery.nstSlider.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/wickedpicker.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/jquery.ajaxchimp.min.js') }}"></script>
+<script src="{{ asset('css/frontend/js/chart.js') }}"></script>
+
+<script src="{{ asset('css/frontend/custom.js') }}"></script>
+<script src="{{ asset('css/frontend/dashboard.js') }}"></script>
+</body>
---
+++ resources/views/frontend/dashboard/sidebar.blade.php
@@ -0,0 +1,48 @@
+<div class="lst-dash-user-profile">
+    <div class="thumb">
+        <img src="{{ asset('images/frontend/dash-author.jpg') }}" class="img-fluid" alt="">
+    </div>
+    <div class="profile-body">
+        <h4>Steve Aldridge</h4>
+        <span>@username</span>
+        <span class="pro"><i class="fas fa-bookmark"></i>Pro User</span>
+    </div>
+</div>
+<div class="lst-dash-side-nav">
+    <ul class="dashboard-nav" id="dashboard-nav">
+        <li class="navigation-item">
+            <a href="dashboard.html" class="active"><i class="fas fa-home"></i>Dashboard</a>
+        </li>
+        <li class="navigation-item">
+            <a href="edit-profile.html"><i class="fas fa-user"></i>Edit Profile</a>
+        </li>
+        <li class="navigation-item dropdown">
+            <a href="#"><i class="fas fa-bolt"></i>My Listing</a>
+            <ul class="dropdown-menu">
+                <li><a href="all-listing.html">All Listing</a></li>
+                <li><a href="all-listing.html">Active Listing</a></li>
+                <li><a href="all-listing.html">Pending Listing</a></li>
+                <li><a href="all-listing.html">Expired Listing</a></li>
+            </ul>
+        </li>
+        <li class="navigation-item">
+            <a href="dashboard-add-listing.html"><i class="fas fa-plus-circle"></i>Add Listing</a>
+        </li>
+        <li class="navigation-item">
+            <a href="bookmarked.html"><i class="fas fa-heart"></i>Bookmarked</a>
+        </li>
+        <li class="navigation-item">
+            <a href="#"><i class="fas fa-check-square"></i>Booking</a>
+        </li>
+        <li class="navigation-item">
+            <a href="message.html"><i class="far fa-comment-alt"></i>Messages</a>
+        </li>
+        <li class="navigation-item">
+            <a href="dashboard-pricing.html"><i class="fas fa-bell"></i>Pricing Table</a>
+        </li>
+        <li class="navigation-item">
+            <a href="review.html"><i class="fas fa-star"></i>Review</a>
+        </li>
+    </ul>
+    <a href="#" class="logout-button">Logout</a>
+</div>
