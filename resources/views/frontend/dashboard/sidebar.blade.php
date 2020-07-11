748c2d8b43968940b872861fe301d7c0306f6eb7
Tushar Imran <tusharimran9909@gmail.com>
2020-07-11 20:56:50 +0600

    added frontend dashboard menu active class

--- resources/views/frontend/dashboard/sidebar.blade.php
+++ resources/views/frontend/dashboard/sidebar.blade.php
@@ -8,27 +8,23 @@
         <h4>{{Auth::user()->name}}</h4>
     </div>
 </div>
-
 <div class="lst-dash-side-nav">
     <ul class="dashboard-nav" id="dashboard-nav">
         <li class="navigation-item">
-            <a href="dashboard.html" class="active"><i class="fas fa-home"></i>Dashboard</a>
+            <a href="{{url("listing/profile")}}" class="{{ (request()->is('listing/profile')) ? 'active' : '' }}"><i class="fas fa-user"></i>Edit Profile</a>
         </li>
         <li class="navigation-item">
-            <a href="{{url("/listing/profile")}}"><i class="fas fa-user"></i>Edit Profile</a>
+            <a href="{{url("listing/all_listing")}}" class="{{ (request()->is('listing/all_listing')) ? 'active' : '' }}"><i class="fas fa-bolt"></i>My Listing</a>
         </li>
         <li class="navigation-item">
-            <a href="{{url("/listing/all_listing")}}"><i class="fas fa-bolt"></i>My Listing</a>
+            <a href="{{url("listing/add")}}" class="{{ (request()->is('listing/add')) ? 'active' : '' }}"><i class="fas fa-plus-circle"></i>Add Listing</a>
         </li>
         <li class="navigation-item">
-            <a href="{{url("/listing/add")}}"><i class="fas fa-plus-circle"></i>Add Listing</a>
+            <a href="{{url("listing/bookmarked")}}" class="{{ (request()->is('listing/bookmarked')) ? 'active' : '' }}"><i class="fas fa-heart"></i>Bookmarked</a>
         </li>
         <li class="navigation-item">
-            <a href="{{url("/listing/bookmarked")}}"><i class="fas fa-heart"></i>Bookmarked</a>
-        </li>
-        <li class="navigation-item">
-            <a href="{{url("/listing/review")}}"><i class="fas fa-star"></i>Review</a>
+            <a href="{{url("listing/review")}}" class="{{ (request()->is('listing/review')) ? 'active' : '' }}"><i class="fas fa-star"></i>Review</a>
         </li>
     </ul>
-    <a href="#" class="logout-button">Logout</a>
+    <a href="{{url("logout")}}" class="logout-button">Logout</a>
 </div>
